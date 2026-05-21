b6ce410a45bbf35da12828849a11e7a5fa12017f
aman9768 <aman983322@gmail.com>
2026-05-21 22:02:56 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Gen AI Project",
+    version='0.1',
+    author="Aman",
+    author_email="aman983322@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
