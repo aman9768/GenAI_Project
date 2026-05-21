b69a6367ff9f662bb09ff826f172f5d3f63f24c4
aman9768 <aman983322@gmail.com>
2026-05-22 00:08:03 +0530

    completed

---
+++ app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+# import time
+from src.helper import get_pdf_text,get_text_chunks,get_vector_store,get_conversational_chain
+
+def user_input(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chatHistory = response['chat_history']
+    for i, message in enumerate(st.session_state.chatHistory):
+        if i%2 == 0:
+            st.write("User: ", message.content)
+        else:
+            st.write("Reply: ", message.content)
+
+def main():
+    st.set_page_config("Information Retrieval")
+    st.header("Information Retrieval System")
+    
+    user_question=st.text_input("ask a question from the pdf files")
+    
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chatHistory" not in st.session_state:
+        st.session_state.chatHistory = None
+    if user_question:
+        user_input(user_question)
+    
+    
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs=st.file_uploader("Upload your PDF files",accept_multiple_files=True)
+        if st.button("submit"):
+            with st.spinner("processing"):
+                raw_text=get_pdf_text(pdf_docs)
+                text_chunks=get_text_chunks(raw_text)
+                vector_store=get_vector_store(text_chunks)
+                st.session_state.conversation=get_conversational_chain(vector_store)
+                # time.sleep(3)
+                st.success("done")
+    
+if __name__=="__main__":
+    main()
